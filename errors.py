5cbee6c746baf303109c0a95ce5b7278634b69f5
sayed-imran <sayedimran00786@gmail.com>
2025-03-31 19:44:45 +0530

    Add custom exceptions for annotation handling

---
+++ errors.py
@@ -0,0 +1,7 @@
+class AnnotationDoesNotExist(Exception):
+    """Exception raised when an annotation does not exist."""
+    pass
+
+class InvalidAnnotationValue(Exception):
+    """Exception raised when an annotation value is invalid."""
+    pass
