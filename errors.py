94e12d970fcc85f28c86bb6e49e626f5952f810b
sayed-imran <sayedimran00786@gmail.com>
2025-04-03 12:51:52 +0530

    Add custom exceptions for issuer and cluster issuer not existing

--- errors.py
+++ errors.py
@@ -5,3 +5,11 @@ class AnnotationDoesNotExist(Exception):
 class InvalidAnnotationValue(Exception):
     """Exception raised when an annotation value is invalid."""
     pass
+
+class IssuerDoesnotExist(Exception):
+    """Exception raised when an issuer does not exist."""
+    pass
+
+class ClusterIssuerDoesnotExist(Exception):
+    """Exception raised when a cluster issuer does not exist."""
+    pass
